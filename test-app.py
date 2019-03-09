ca59f37a3e6ed593523ef4fd1e137e36d11d1151
Tomoaki Sugiayma <sti320a@gmail.com>
2019-03-09 23:14:13 +0900

    Updated testcase

--- test-app.py
+++ test-app.py
@@ -2,16 +2,74 @@ from app import app
 import os
 import unittest
 
-class BasicTest(unittest.TestCase):
+class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
-        tester = app.test_client(self)
+        tester = app.app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_database(self):
         tester = os.path.exists('test.db')
-        self.assertTrue(tester)
+        self.assertTrue(tester, True)
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, app.app.config['DATABASE'] = tempfile.mkstemp()
+        app.app.config['TESTING'] = True
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(app.app.config['DATABASE'])
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password 
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+    
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
+
+    def test_login_logout(self):
+        rv = self.login(
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD']
+        ) 
+        assert b'You were logged in' in rv.data
+        rv = self.logout()
+        assert b'You were logged out' in rv.data
+        rv = self.login(
+            app.app.config['USERNAME'] + 'x',
+            app.app.config['PASSWORD']
+        )
+        assert b'Invalid username' in rv.data
+        rv = self.login(
+            app.app.config['USERNAME'] + 'x',
+            app.app.config['PASSWORD']
+        )
+        assert b'Invalid password' in rv.data
+
+    def test_messages(self):
+        self.login(
+            app.app.config['USERNAME'],
+            app.app.config['PASSWORD']
+        )
+        rv = self.app.post('/add', data=dict(
+            title='<Hello>',
+            text='<strong>HTML</strong> allowed here'
+        ), follow_redirects=True)
+        assert b'No entries here so far' not in rv.data
+        assert b'&lt;Hello&gt;' in rv.data
+        assert b'<strong>HTML</strong> allowed here' in rv.data
 
 
 if __name__ == '__main__':
